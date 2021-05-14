0765e993dce9c0cfbef2f4ec44389b024bf0dbe2
stavroskapris <stavroskapridakis@gmail.com>
2021-05-14 17:26:15 +0300

    set proxy

--- driver-config/index.js
+++ driver-config/index.js
@@ -1,13 +1,21 @@
 const { Builder } = require("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox")
+const proxy = require("selenium-webdriver/proxy")
 const options = new firefox.Options();
-
+const proxyServer = "" //todo paste a proxy ip here 
 options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads")
 options.setPreference("browser.download.folderList", 2)
 options.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/x-csv")
 
 
 
-const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+const driver = new Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(options)
+    .setProxy(proxy.manual({
+        http: proxyServer,
+        https: proxyServer,
+    }))
+    .build();
 
 driver.get("https://www.spitogatos.gr")
