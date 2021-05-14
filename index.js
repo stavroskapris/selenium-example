c464aeb3e53d91350be118ccc18e496b8e2f8ba9
stavroskapris <stavroskapridakis@gmail.com>
2021-05-14 15:52:59 +0300

    example script, google search

---
+++ index.js
@@ -0,0 +1,12 @@
+// import { Builder, By, Key, util } from "selenium-webdriver";
+const { Builder, By, Key, util } = require("selenium-webdriver");
+
+async function example() {
+    let driver = await new Builder().forBrowser('firefox').build();
+
+    await driver.get("http://google.com");
+
+    await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+}
+
+example();
